067fbe11604a5717d7efb8cf3db52b47bfcf0e26
ritwikranjan <ritwikranjan99@gmail.com>
2020-06-03 02:40:56 +0530

    built index page

--- hackerrank/questionbank/views.py
+++ hackerrank/questionbank/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+
+from .models import Category
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+class CategoryList(generic.ListView):
+    template_name = 'questionbank/index.html'
+    def get_queryset(self):
+        return Category.objects.all()
+    
     
